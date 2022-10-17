f6bee6a93dd8f2d2a2774bec6b2ebd6ba6e9697c
LC1A_03_KokiAbe <k020g1182@g.neec.ac.jp>
2022-10-17 14:16:22 +0900

    02_03

---
+++ Input.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <windows.h>
+#include <wrl.h>
+#define DIRECTINPUT_VERSION     0x0800
+#include <dinput.h>
+
+
+class Input
+{
+public:
+	// namespace省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+public:	// メンバ関数
+	// 初期化
+	void Initialize(HINSTANCE hInstance, HWND hwnd);
+
+	// 更新
+	void Update();
+private: // メンバ変数
+	ComPtr<IDirectInputDevice8> keyboard;
+};
